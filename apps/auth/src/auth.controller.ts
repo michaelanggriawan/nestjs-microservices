c330823fed28df1ffa022a71e9972ebc926aae3a
michaelanggriawan <michaelanggriawan941@gmail.com>
2022-06-06 23:37:53 +0800

    fix: remove console

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -25,7 +25,6 @@ export class AuthController {
   @UseGuards(JwtAuthGuard)
   @MessagePattern('validate_user')
   async validateUser(@CurrentUser() user: User) {
-    console.log('show error 3');
     return user;
   }
 }
