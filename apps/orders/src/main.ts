b7378431cfdc3514a110816075cd9972e6a0c263
michaelanggriawan <michaelanggriawan941@gmail.com>
2022-06-09 20:10:06 +0800

    fix: typo

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
-// main.ts
+
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
   app.useGlobalPipes(new ValidationPipe());
