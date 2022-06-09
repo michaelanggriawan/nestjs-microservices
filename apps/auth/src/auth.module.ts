bb4847aeb46d34435c86ce1cf666210650c93f82
michaelanggriawan <michaelanggriawan941@gmail.com>
2022-06-09 22:32:22 +0800

    fix: relocate folder coverage

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -8,7 +8,7 @@ import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UsersModule } from './users/users.module';
-
+// module
 @Module({
   imports: [
     DatabaseModule,
