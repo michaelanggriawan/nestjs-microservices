ac3abee5f52fa20de0fc8300cddc4a80a94d1cd9
michaelanggriawan <michaelanggriawan941@gmail.com>
2022-06-14 11:24:45 +0800

    chore: split sonar test coverage

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -18,7 +18,7 @@ export class AuthController {
     @Res({ passthrough: true }) response: Response,
   ) {
     await this.authService.login(user, response);
-    response.send(user);
+    // response.send(user);
     return user;
   }
 
