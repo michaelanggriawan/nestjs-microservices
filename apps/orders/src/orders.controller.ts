9696daabb975e31b8e7f3bc35763e505c1db7c8a
michaelanggriawan <michaelanggriawan941@gmail.com>
2022-06-14 15:59:29 +0800

    fix: conflict

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderRequest } from './dto/create-order.request';
 import { JwtAuthGuard } from '@app/common';
-// orderss
+// ordersss
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
